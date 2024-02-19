3549f492cbed69bb166c21996cd2175726be7753
David wanjohi <davidwagura83@gmail.com>
2024-02-19 19:21:20 +0300

    add styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,17 @@
   <div class="home">
     <div class="feature-card">
       <router-link to="/movie/tt0409591">
-        <img src="https://imgs.search.brave.com/Fu1VJE00vliAmhVZj3soe1bCML2AQwaVb7OnMTWcLmA/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/ODFmSnZTRXNlc0wu/anBn" 
-        alt="Naruto Poster" class="featured-img" />
+        <img src="https://m.media-amazon.com/images/M/MV5BZmQ5NGFiNWEtMmMyMC00MDdiLTg4YjktOGY5Yzc2MDUxMTE1XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg" alt="Naruto Poster" class="featured-img" />
         <div class="detail">
           <h3>Naruto</h3>
-          <p>Naruto Uzumaki is the main character of the beloved Naruto series and he is a resident of the Hidden Leaf Village in the Land of Fire. </p>
+          <p>Naruto Uzumaki, a mischievous adolescent ninja, struggles as he searches for recognition and dreams of becoming the Hokage, the village's leader and strongest ninja.</p>
         </div>
       </router-link>
     </div>
 
-    <form @submit.prevent="SearchMovies()" class="search-box" name="search-form">
-      <input type="text" placeholder="what are you looking for?" v-model="search"/>
-      <input type="submit" value="Search">
+    <form @submit.prevent="SearchMovies()" class="search-box">
+      <input type="text" placeholder="What are you looking for?" v-model="search" />
+      <input type="submit" value="Search" />
     </form>
 
     <div class="movies-list">
@@ -21,93 +20,90 @@
         <router-link :to="'/movie/' + movie.imdbID" class="movie-link">
           <div class="product-image">
             <img :src="movie.Poster" alt="Movie Poster" />
-            <div class="type">
-              {{ movie.type }}
-            </div>
-            <div class="detail">
-              <p class="y">{{ movie.year }}</p>
-              <h3>{{ movie.title }}</h3>
-            </div>
+            <div class="type">{{ movie.Type }}</div>
           </div>
-      </router-link> 
+          <div class="detail">
+            <p class="year">{{ movie.Year }}</p>
+            <h3>{{ movie.Title }}</h3>
+          </div>
+        </router-link>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import  {ref} from 'vue';
-  import env from '@/env.js'
+import { ref } from 'vue';
+import env from '@/env.js'
 
-  export default{
-    setup () {
-      const search = ref("");
-      const movies = ref([]);
+export default {
+  setup () {
+    const search = ref("");
+    const movies = ref([]);
 
-      const SearchMovies = () => {
-        if (search.value != "") {
-
-          fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&s=$(search.value)`)
-            .then(response => response.json())
-            .then(data => {
-              movies.value = data.Search;
-              search.value = "";
-
-            });
-        }
+    const SearchMovies = () => {
+      if (search.value != "") {
+        fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
+          .then(response => response.json())
+          .then(data => {
+            movies.value = data.Search;
+            search.value = "";
+          });
       }
-      return {
-        search,
-        movies,
-        SearchMovies
+    }
+
+    return {
+      search,
+      movies,
+      SearchMovies
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.home {
+  .feature-card {
+    position: relative;
+
+    .featured-img {
+      display: block;
+      width: 100%;
+      height: 300px;
+      object-fit: cover;
+
+      position: relative;
+      z-index: 0;
+    }
+
+    .detail {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.6);
+      padding: 16px;
+      z-index: 1;
+
+      h3 {
+        color:#FFF;
+        margin-bottom: 16px;
+      }
+
+      p {
+        color: #FFF;
       }
     }
   }
 
-</script>
- <style lang="scss">
-  .home {
+  .search-box {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 16px;
 
-    .feature-card {
-      position: relative;
-
-      .featured-img {
-        display: block;
-        width: 100%;
-        height: 300px;
-        object-fit: cover;
-
-        position: relative;
-        z-index: 0;
-      }
-
-      .detail {
-        position: absolute;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: rgba(0, 0, 0, 0.6);
-        padding: 16px;
-        z-index: 1;
-
-        h3 {
-          color: #FFF;
-          margin-bottom: 16px;
-        }
-
-        p {
-          color: #FFF;
-        }
-      }
-    }
-    .search-box {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      padding: 16px;
-    
-      input {
+    input {
       display: block;
       appearance: none;
       border: none;
@@ -125,28 +121,88 @@
         transition: 0.4s;
 
         &::placeholder {
-          color: #f3f3f3
+          color: #f3f3f3;
         }
+
         &:focus {
           box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.2);
         }
       }
+
       &[type="submit"] {
         width: 100%;
         max-width: 300px;
-        background-color: rgb(57, 117, 57);
+        background-color: #42B883;
         padding: 16px;
         border-radius: 8px;
-        color: white;
+        color: #FFF;
         font-size: 20px;
         text-transform: uppercase;
         transition: 0.4s;
 
         &:active {
-          background-color: #3b8070;
+          background-color: #3B8070;
         }
       }
     }
-  } 
+  }
+
+  .movies-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0px 8px;
+
+    .movie {
+      max-width: 50%;
+      flex: 1 1 50%;
+      padding: 16px 8px;
+
+      .movie-link {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+
+        .product-image {
+          position: relative;
+          display: block;
+
+          img {
+            display: block;
+            width: 100%;
+            height: 275px;
+            object-fit: cover;
+          }
+
+          .type {
+            position: absolute;
+            padding: 8px 16px;
+            background-color: #42B883;
+            color: #FFF;
+            bottom: 16px;
+            left: 0px;
+            text-transform: capitalize;
+          }
+        }
+
+        .detail {
+          background-color: #496583;
+          padding: 16px 8px;
+          flex: 1 1 100%;
+          border-radius: 0px 0px 8px 8px;
+
+          .year {
+            color: #AAA;
+            font-size: 14px;
+          }
+
+          h3 {
+            color: #FFF;
+            font-weight: 600;
+            font-size: 18px;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
