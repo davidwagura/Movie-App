8228cfea7e8ee9a1499e4186cc6d099af2cf743d
David wanjohi <davidwagura83@gmail.com>
2024-08-03 19:19:27 +0300

    Modify code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,53 +1,89 @@
 <template>
   <div class="home">
-    <form @submit.prevent="SearchMovies()" class="search-box">
-      <input type="text" placeholder="What are you looking for?" v-model="search" />
-      <input type="submit" value="Search" />
-    </form>
+    <div class="search-box">
+      <input
+        type="text"
+        v-model="search"
+        placeholder="Search for a movie..."
+        @click="SearchMovies"
+      />
+    </div>
 
-    <div v-if="movie" class="movie-details">
-      <div class="feature-card">
-        <img :src="movie.Poster" :alt="movie.Title + ' Poster'" class="featured-img" />
-        <div class="detail">
-          <h3>{{ movie.Title }}</h3>
-          <p>{{ movie.Plot }}</p>
-          <p><strong>Director:</strong> {{ movie.Director }}</p>
-          <p><strong>Actors:</strong> {{ movie.Actors }}</p>
-          <p><strong>Genre:</strong> {{ movie.Genre }}</p>
-          <p><strong>Released:</strong> {{ movie.Released }}</p>
-          <p><strong>Runtime:</strong> {{ movie.Runtime }}</p>
-          <p><strong>Language:</strong> {{ movie.Language }}</p>
-          <p><strong>Country:</strong> {{ movie.Country }}</p>
-          <p><strong>Awards:</strong> {{ movie.Awards }}</p>
-        </div>
+    <div v-if="loading" class="loading">Loading...</div>
+    <div v-if="error" class="error">{{ error }}</div>
+
+    <div v-if="movie" class="movie-card">
+      <img :src="movie.imageurl[0]" alt="Movie Poster" class="movie-poster"/>
+      <div class="movie-details">
+        <h3 class="movie-title">{{ movie.title }}</h3>
+        <p class="movie-genres">{{ movie.genre.join(', ') }}</p>
+        <p class="movie-release">Released: {{ movie.released }}</p>
+        <p class="movie-rating">IMDB Rating: {{ movie.imdbrating }}</p>
+        <p class="movie-synopsis">{{ movie.synopsis }}</p>
       </div>
     </div>
   </div>
 </template>
 <script setup>
-import { ref } from 'vue';
-import env from '@/env.js';
+import { ref, watch } from 'vue';
+// import env from '@/env.js';
 
 const search = ref("");
 const movie = ref(null);
+const error = ref(null);
+const loading = ref(false);
 
-const SearchMovies = () => {
-  if (search.value !== "") {
-    fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&t=${search.value}`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        if (data.Response === "True") {
-          movie.value = data;
-        } else {
-          movie.value = null;
-        }
-        search.value = "";
-      });
+// Function to debounce the search input
+// const debounce = (func, delay) => {
+//   let timer;
+//   return (...args) => {
+//     clearTimeout(timer);
+//     timer = setTimeout(() => func(...args), delay);
+//   };
+// };
+
+const SearchMovies = async () => {
+  if (search.value.trim() === "") return;
+
+  loading.value = true;
+  error.value = null;
+  movie.value = null;
+
+  try {
+    const response = await fetch(`https://ott-details.p.rapidapi.com/${search.value.trim()}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'x-rapidapi-ua': 'RapidAPI-Playground',
+        'x-rapidapi-key': 'e7494e734amshde50a14f5967d05p13138ejsn93c07c5317f8',
+        'x-rapidapi-host': 'ott-details.p.rapidapi.com'
+      }
+    });
+
+    console.log(response);
+    
+    const data = await response.json();
+    if (data.Response === "True") {
+      movie.value = data;
+    } else {
+      error.value = data.Error;
+    }
+  } catch (err) {
+    error.value = "An error occurred while fetching the movie details.";
+  } finally {
+    loading.value = false;
+    search.value = "";
   }
 };
 
+// Debounce the search function to avoid too many API calls
+// const debounceSearch = debounce(SearchMovies, 300);
+
+// Watch for changes in the search input and trigger debounced search
+watch(search);
 </script>
+
 <style lang="scss">
 .home {
   .feature-card {
@@ -128,6 +164,20 @@ const SearchMovies = () => {
         }
       }
     }
+  }
+
+  .loading {
+    text-align: center;
+    font-size: 20px;
+    color: #42B883;
+    margin-top: 20px;
+  }
+
+  .error {
+    text-align: center;
+    font-size: 20px;
+    color: red;
+    margin-top: 20px;
   }
 }
 </style>
